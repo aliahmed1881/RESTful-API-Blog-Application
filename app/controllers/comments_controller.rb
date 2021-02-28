ec2e53542f5a5be5df44ab54ee1ad0e0d7532823
Mohammed Ahmed Sleem <71722783+aliahmed1881@users.noreply.github.com>
2021-02-28 21:53:29 +0200

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,55 +4,52 @@ class CommentsController < ApplicationController
 
 
 	def index
-		@comments = Comment.all 
-		
+	     @comments = Comment.all 
 	end
 
 	def show
-			@comment 
+	    @comment 
 	end
 
-  def create
-    return if @post.nil?
-    comment = post.comments.create(comment_params.merge(user_id: current_user.id))
-    respond_with post, comment
-  end
+        def create
+            return if @post.nil?
+            comment = post.comments.create(comment_params.merge(user_id: current_user.id))
+            respond_with post, comment
+        end
 
 
-  def update
-  	if current_user.id == @comment.user_id 
-  			respond_to do |format|
-      		if @comment.update(comment_params)
-      		  format.json { render :show, status: :ok, location: @comment}
-      		else
-      		  format.json { render json: @comment.errors, status: :unprocessable_entity }
-      		end
-    		end
-  	end
-
-  end
+        def update
+  	    if current_user.id == @comment.user_id 
+  		respond_to do |format|
+      		     if @comment.update(comment_params)
+      		           format.json { render :show, status: :ok, location: @comment}
+      		     else
+      		           format.json { render json: @comment.errors, status: :unprocessable_entity }
+      		     end
+    		  end
+  	   end
+       end
 
   
  	def destroy
-  	if current_user.id == @comment.user_id
-     
-        @comment.destroy
-        respond_to do |format|
-          format.json { head :no_content }
-        end
-    end
-  end
+  	  if current_user.id == @comment.user_id
+                @comment.destroy
+            respond_to do |format|
+                format.json { head :no_content }
+             end
+          end
+         end
  
 
 
 private
 
 	def set_comment
-      @comment = Comment.find(params[:id])
-  end
+               @comment = Comment.find(params[:id])
+         end
 
-  def comment_params
-    params.require(:comment).permit(:body)
-  end
+         def comment_params
+             params.require(:comment).permit(:body)
+         end
 
 end
